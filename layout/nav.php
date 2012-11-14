95e8a72b2857770e2c53b70c9391aea3c3934c95
chrisramey <cjramey@gmail.com>
2012-11-13 19:02:00 -0600

    Added file & form code snippets, removed responsive design

--- layout/nav.php
+++ layout/nav.php
@@ -26,7 +26,7 @@ $nav = array(
 	)
 );
 ?>
-<div id="navbar" class="navbar navbar-static affix-top desktop-only"  data-spy="affix" data-offset-top="0">
+<div id="navbar" class="navbar navbar-static affix-top"  data-spy="affix" data-offset-top="0">
 	<div class="navbar-inner">
 		<div class="container">
 			<a href="./" class="brand">PHP Reference</a>
@@ -44,21 +44,5 @@ $nav = array(
 			</ul>
 			<!--<a class="pull-right" href="http://www.php.net/manual/en/" target="_blank"><img src="img/php-icon.png" alt="PHP logo" /></a>-->
 		</div>
-	</div>
-</div>
-
-<div id="nav-phone" class="navbar navbar-static affix-top phone-only" data-spy="affix" data-offset-top="0">
-	<div class="btn-group pull-right">
-		<a class="btn dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-th-list"></i> Menu</a>
-		<ul class="dropdown-menu">
-			<?php foreach($nav as $cat => $pages): ?><li>
-				<li class="dropdown">
-					<li class="nav-header"><?php echo $cat ?></li>
-					<?php foreach($pages as $name => $id): ?>
-						<li><a href="./?ref=<?php echo $id ?>"><?php echo $name ?></a></li>
-					<?php endforeach ?>
-				</li>
-			<?php endforeach ?>
-		</ul>
 	</div>
 </div>
